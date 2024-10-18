ba7315c8dcfb17516c52662dd940bca32025bcd1
Anthony Gab <dev@agabt.com>
2024-10-18 15:44:49 +0200

    Added all files. Vite project. Pnpm

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+import stylisticJs from "@stylistic/eslint-plugin-js";
+
+export default [
+  {
+    plugins: {
+      "@stylistic/js": stylisticJs
+    },
+
+    rules: {
+      "@stylistic/js/semi": ["warn", "always"],
+      "@stylistic/js/quotes": ["warn", "double", { "allowTemplateLiterals": true }],
+    }
+  }
+];
