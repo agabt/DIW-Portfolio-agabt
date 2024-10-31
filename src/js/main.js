4f17fd8486e20d8f44c0a24fdb9c9f43669efeea
Anthony Gab <dev@agabt.com>
2024-10-31 19:48:01 +0100

    Added JS to manage the tabs

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,20 @@
-console.log("hola");
+import { $, $all } from "./querySelectors.js";
+
+const hash = window.location.hash === "" ? "#home" : window.location.hash;
+let $activeSection = $(hash);
+let $activeNavItem = $(`${hash}-nav`);
+$activeSection.classList.add("active");
+$activeNavItem.classList.add("active-nav");
+
+window.addEventListener("hashchange", () => {
+	const newHash = window.location.hash;
+	$activeSection.classList.remove("active");
+	$activeNavItem.classList.remove("active-nav");
+
+	const $newActiveSection = $(newHash);
+	const $newActiveNavItem = $(`${newHash}-nav`);
+	$newActiveSection.classList.add("active");
+	$newActiveNavItem.classList.add("active-nav");
+	$activeSection = $newActiveSection;
+	$activeNavItem = $newActiveNavItem;
+});
