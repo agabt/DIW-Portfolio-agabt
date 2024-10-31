5ad8e8a6e7c2f2827867df7388bee9d864b67a4c
Anthony Gab <dev@agabt.com>
2024-10-31 19:47:12 +0100

    Project configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,18 @@
 import stylisticJs from "@stylistic/eslint-plugin-js";
 
 export default [
-  {
-    plugins: {
-      "@stylistic/js": stylisticJs
-    },
+	{
+		plugins: {
+			"@stylistic/js": stylisticJs,
+		},
 
-    rules: {
-      "@stylistic/js/semi": ["warn", "always"],
-      "@stylistic/js/quotes": ["warn", "double", { "allowTemplateLiterals": true }],
-    }
-  }
+		rules: {
+			"@stylistic/js/semi": ["warn", "always"],
+			"@stylistic/js/quotes": [
+				"warn",
+				"double",
+				{ allowTemplateLiterals: true },
+			],
+		},
+	},
 ];
